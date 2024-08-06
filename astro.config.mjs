4076ec1ccaa7eee62b224d5eba6c39da38d149af
mdhowey <dev.howey@gmail.com>
2024-08-06 13:52:06 -0600

    feat(web): add email worker and resend

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
-
-import alpinejs from "@astrojs/alpinejs";
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), compress(), alpinejs()],
+  integrations: [tailwind(), compress()],
+  output: 'server',
+  adapter: cloudflare(),
 });
---
+++ src/pages/api/sendEmail.json.ts
@@ -0,0 +1,57 @@
+// https://developers.cloudflare.com/developer-spotlight/tutorials/handle-form-submission-with-astro-resend/ 
+
+export const prerender = false; //This will not work without this line
+
+import type { APIRoute } from 'astro';
+import { Resend } from 'resend';
+
+const resend = new Resend(import.meta.env.RESEND_API_KEY);
+
+export const POST: APIRoute = async ({ request }) => {
+  const data = await request.formData();
+  const name = data.get('name');
+  const email = data.get('email');
+  const phone = data.get('phone');
+  const message = data.get('message'); // Validate the data - making sure values are not empty
+
+  if (!name || !email || !message) {
+    return new Response(
+      JSON.stringify({
+        message: `Fill out all fields.`,
+      }),
+      {
+        status: 404,
+        statusText: 'Did not provide the right data',
+      }
+    );
+  } // Sending information to Resend
+
+  const sendResend = await resend.emails.send({
+    from: 'support@resend.dev',
+    to: 'michael@hammerfist.studio',
+    subject: `Sumbission from ${name}`,
+    html: `<p>Hi ${name},</p><p>Your message was received.</p>`,
+  }); // If the message was sent successfully, return a 200 response
+
+  if (sendResend.data) {
+    return new Response(
+      JSON.stringify({
+        message: `Message successfully sent!`,
+      }),
+      {
+        status: 200,
+        statusText: 'OK',
+      }
+    ); // If there was an error sending the message, return a 500 response
+  } else {
+    return new Response(
+      JSON.stringify({
+        message: `Message failed to send: ${sendResend.error}`,
+      }),
+      {
+        status: 500,
+        statusText: `Internal Server Error: ${sendResend.error}`,
+      }
+    );
+  }
+};
